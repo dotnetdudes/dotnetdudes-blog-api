6dfb19c39c525b7b89d0b3990e84e20337d00486
John Morton <johnmorton503@gmail.com>
2023-11-19 18:48:24 +0800

    add tests stubs

--- BlogApiTests.cs
+++ BlogApiTests.cs
@@ -43,5 +43,396 @@ public class BlogApiTests
         Assert.Equal(2, posts.Count());
     }
 
+    [Fact]
+    public async Task GetPost_ReturnsPost()
+    {
+        // Arrange
+        var mockPost = new Post();
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var post = await response.Content.ReadAsAsync<Post>();
+        Assert.Equal(mockPost, post);
+    }
+
+    [Fact]
+    public async Task GetPost_ReturnsNotFound()
+    {
+        // Arrange
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((Post)null);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsPostWithComments()
+    {
+        // Arrange
+        var mockPost = new Post();
+        var mockComments = new List<Comment> { new Comment(), new Comment() };
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockComments);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var post = await response.Content.ReadAsAsync<Post>();
+        Assert.Equal(mockPost, post);
+        Assert.Equal(2, post.Comments.Count());
+    }   
+
+    [Fact]
+    public async Task GetPostComments_ReturnsNotFound()
+    {
+        // Arrange
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((Post)null);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsPostWithNoComments()
+    {
+        // Arrange
+        var mockPost = new Post();
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(new List<Comment>());
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var post = await response.Content.ReadAsAsync<Post>();
+        Assert.Equal(mockPost, post);
+        Assert.Empty(post.Comments);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsPostWithCommentsForPost()
+    {
+        // Arrange
+        var mockPost = new Post();
+        var mockComments = new List<Comment> { new Comment(), new Comment() };
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockComments);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var post = await response.Content.ReadAsAsync<Post>();
+        Assert.Equal(mockPost, post);
+        Assert.Equal(2, post.Comments.Count());
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsPostWithCommentsForPostWithNoComments()
+    {
+        // Arrange
+        var mockPost = new Post();
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(new List<Comment>());
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var post = await response.Content.ReadAsAsync<Post>();
+        Assert.Equal(mockPost, post);
+        Assert.Empty(post.Comments);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsNotFoundForPost()
+    {
+        // Arrange
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((Post)null);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsNotFoundForComments()
+    {
+        // Arrange
+        var mockPost = new Post();
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((IEnumerable<Comment>)null);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        }).CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsNotFoundForPostAndComments()
+    {
+        // Arrange
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((Post)null);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((IEnumerable<Comment>)null);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);
+            });
+        })
+        .CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsPostWithCommentsForPostWithComments()
+    {
+        // Arrange
+        var mockPost = new Post();
+        var mockComments = new List<Comment> { new Comment(), new Comment() };
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockComments);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);                
+            });
+        })
+        .CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var post = await response.Content.ReadAsAsync<Post>();
+        Assert.Equal(mockPost, post);
+        Assert.Equal(2, post.Comments.Count());
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsPostWithCommentsForPostWithNoComments()
+    {
+        // Arrange
+        var mockPost = new Post();
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(new List<Comment>());
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);                
+            });
+        })
+        .CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var post = await response.Content.ReadAsAsync<Post>();
+        Assert.Equal(mockPost, post);
+        Assert.Empty(post.Comments);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsNotFoundForPost()
+    {
+        // Arrange
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((Post)null);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);                
+            });
+        })
+        .CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsNotFoundForComments()
+    {
+        // Arrange
+        var mockPost = new Post();
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((IEnumerable<Comment>)null);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);                
+            });
+        })
+        .CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsNotFoundForPostAndComments()
+    {
+        // Arrange
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((Post)null);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync((IEnumerable<Comment>)null);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);                
+            });
+        })
+        .CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPostComments_ReturnsPostWithCommentsForPostWithComments()
+    {
+        // Arrange
+        var mockPost = new Post();
+        var mockComments = new List<Comment> { new Comment(), new Comment() };
+        _mockDbConnection.Setup(db => db.QueryFirstOrDefaultAsync<Post>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockPost);
+        _mockDbConnection.Setup(db => db.QueryAsync<Comment>(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockComments);
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _mockDbConnection.Object);                
+            });
+        })
+        .CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/posts/1/comments");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var post = await response.Content.ReadAsAsync<Post>();
+        Assert.Equal(mockPost, post);
+        Assert.Equal(2, post.Comments.Count());
+    }
+
     // Repeat the above test method for each endpoint in your API, adjusting the Arrange, Act, and Assert steps as necessary.
 }
