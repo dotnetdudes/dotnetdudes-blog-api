64506ecc585b1edb6a299b959b2412866ec7d356
John Morton <johnmorton503@gmail.com>
2023-11-27 23:37:30 +0800

    add test stubs

---
+++ Dotnetdudes.Web.Blog.Api.Tests/BlogEndpointsV1Tests.cs
@@ -0,0 +1,12 @@
+﻿using Moq;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Dotnetdudes.Web.Blog.Api.Tests
+{
+    public class BlogEnpointsV1Tests
+    {
+        // generate tests for BlogEndpointsV1.cs
+
+    }
+}
--- Dotnetdudes.Web.Blog.Api.Tests/PostTests.cs
+++ Dotnetdudes.Web.Blog.Api.Tests/PostTests.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+using Xunit;
+using Dotnetdudes.Web.Blog.Api.Models;
+using Dotnetdudes.Web.Blog.Api.Routes;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,8 @@ using System.Threading.Tasks;
 
 namespace Dotnetdudes.Web.Blog.Api.Tests
 {
-    internal class PostTests
+    public class PostTests
     {
+        // generate tests for Program.cs
     }
 }
